9228e386ea2d373ab9fc2a40e0c495d70c6d0688
Marinsan <cristianmarin@iesebre.com>
2018-09-29 16:34:56 +0200

    WIP

---
+++ app/Http/Controllers/Lessons.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marin
+ * Date: 29/09/18
+ * Time: 16:24
+ */
+
+namespace App\Http\Controllers;
+
+
+class Lessons
+{
+
+}
---
+++ app/Http/Controllers/People.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marin
+ * Date: 29/09/18
+ * Time: 16:22
+ */
+
+namespace App\Http\Controllers;
+
+
+class People
+{
+
+}
