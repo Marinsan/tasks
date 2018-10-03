32990e2e8f31ca7dce0c4f19949c61c6dffe0bc7
Marinsan <cristianmarin@iesebre.com>
2018-10-03 18:01:51 +0200

    WIP

---
+++ vue-tasks/src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li>Tasca 1</li>
+        <li>Tasca 2</li>
+        <li>Tasca 3</li>
+        <li>Tasca 4</li>
+        <li>Tasca 5</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "Tasks"
+    }
+</script>
+
+<style scoped>
+
+</style>
