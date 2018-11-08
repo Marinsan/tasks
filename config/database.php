a6f613671031417ffa89d9a9316038b00b8cee6d
Marinsan <cristianmarin@iesebre.com>
2018-11-08 20:11:49 +0100

    WIP

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', ''),
+            'database' => env('DB_ROOT_DATABASE', ''),
             'username' => env('DB_ROOT_USERNAME', 'root'),
             'password' => env('DB_ROOT_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
