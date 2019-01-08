80210cd6f965ea2ae1f047263d965759844d3cfb
Marinsan <cristianmarin@iesebre.com>
2019-01-08 19:30:22 +0100

    botons :fire:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,14 @@
             <v-toolbar-title> Cristian Marin Tejeda</v-toolbar-title>
             <v-spacer></v-spacer>
             @if (Route::has('login'))
-                <div class="top-right links">
+                
                     @auth
                         <v-btn flat href="{{ url('/home') }}">Home</v-btn>
                     @else
                         <v-btn flat href="{{ route('login') }}">Login</v-btn>
                         <v-btn flat href="{{ route('register') }}">Register</v-btn>
                     @endauth
-                </div>
+
             @endif
         </v-toolbar>
         <v-content>
