00f08b105860c64d4c252fbc849633d77cc228c1
Marinsan <cristianmarin@iesebre.com>
2019-01-10 21:29:40 +0100

    wip

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="user" content="{{ logged_user() }}">
     <meta name="git" content="{{ git() }}">
-    <meta name="theme-color" content="#2BB0ED"/>
+    <meta name="theme-color" content="primary"/>
     <link rel="manifest" href="/manifest.json">
     <link rel="shortcut icon" type="image/png" href="https://batdongsanngach.com/wp-content/uploads/2018/01/checklist-icon-350-28.png"/>
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
--- resources/views/layout/login.blade.php
+++ resources/views/layout/login.blade.php
@@ -8,7 +8,7 @@
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
     <link rel="shortcut icon" type="image/png" href="https://batdongsanngach.com/wp-content/uploads/2018/01/checklist-icon-350-28.png"/>
     <link rel="manifest" href="/manifest.json">
-    <meta name="theme-color" content="#2BB0ED"/>
+    <meta name="theme-color" content="primary"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <style>
         [v-cloak] {display: none}
