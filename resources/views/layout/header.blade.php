439d9d803b058f7685b7515f43dfabe44161b4f1
Marinsan <cristianmarin@iesebre.com>
2019-02-15 12:05:15 +0100

    Sync

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -19,19 +19,19 @@
 
 
     <!-- IOS -->
-    <link rel="apple-touch-icon" sizes="57x57" href="/img/faviconapple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/img/faviconapple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/img/faviconapple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/img/faviconapple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="60x60" href="/img/faviconapple-touch-icon-60x60.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="/img/faviconapple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/img/faviconapple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="/img/faviconapple-touch-icon-152x152.png" />
-    <link rel="icon" type="image/png" href="/img/faviconfavicon-196x196.png" sizes="196x196" />
-    <link rel="icon" type="image/png" href="/img/faviconfavicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/png" href="/img/faviconfavicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/img/faviconfavicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="/img/faviconfavicon-128.png" sizes="128x128" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/img/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="60x60" href="/img/favicon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/img/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-128.png" sizes="128x128" />
     <meta name="application-name" content="&nbsp;"/>
     <meta name="msapplication-TileColor" content="#FFFFFF" />
     <meta name="msapplication-TileImage" content="mstile-144x144.png" />
