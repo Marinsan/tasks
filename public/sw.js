0fa32dacc566778598c283b3931048856c850b4e
Marinsan <cristianmarin@iesebre.com>
2019-03-01 16:08:18 +0100

    Sync

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.761445befd2bb54a967ba93e2b55195d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.d78b8d3639b8338894c115ba6518d6ef.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.skipWaiting()
 workbox.clientsClaim()
